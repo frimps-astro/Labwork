1a3d74ab4199fe58c09d4249d5af2d5386643530
astrobee <frimpsastro@gmail.com>
2021-09-23 11:35:42 +0000

    Code on lab test to display my name

---
+++ displayName.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	string firstname, lastname;
+
+	cout<<"Please enter your first name\n";
+	cin>>firstname;
+
+	cout<<"Please enter your second name\n";
+	cin>>lastname;
+
+	cout<<"Your name is "<<firstname+" "+lastname<<".\n";
+
+	return 0;
+}
