a4b30f123174715b1748ff2af74f5e3b7e654cc4
astrobee <frimpsastro@gmail.com>
2021-09-24 16:19:06 +0000

    Code for labwork 2 question 1

---
+++ enums.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+enum {RED, YELLOW, AMBER=YELLOW, GREEN};
+
+int main(){
+	cout<<RED<<endl;
+	cout<<YELLOW<<endl;
+	cout<<AMBER<<endl;
+	cout<<GREEN<<endl;
+
+	return 0;
+}
