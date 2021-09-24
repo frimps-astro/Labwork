2d6ce0c85d84bd04898c4a91171c4b416185bf82
astrobee <frimpsastro@gmail.com>
2021-09-24 16:19:56 +0000

    Code for labwork 2 question 2 part B

---
+++ literals.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	//data
+	int first {0xf3f2};
+	int second {0437};
+	int third {'a'};
+
+	cout<<first<<" "<<second<<" "<<third<<endl;
+
+}
